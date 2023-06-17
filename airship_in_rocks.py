bdfd149fea2811be7fbb9278cc0244af15a4a76f
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2023-06-17 16:57:50 +0300

    coment
    :P

--- airship_in_rocks.py
+++ airship_in_rocks.py
@@ -4,6 +4,17 @@ pygame.init()
 
 sc = pygame.display.set_mode((800, 500))
 
+def speed_fall():
+    global gravitation, number_gravitation
+    gravitation -= 1
+
+    sc.fill((0, 0, 0))
+    if number_gravitation % 1 == 0:
+        number_gravitation += 1
+        airship.rect.y -= gravitation
+    else:
+        number_gravitation += 1
+
 class AirShip(pygame.sprite.Sprite):
     def __init__(self, x, y, filename):
         self.image = pygame.image.load(filename).convert()
@@ -13,25 +24,26 @@ class AirShip(pygame.sprite.Sprite):
 
 airship = AirShip(50, 250, 'airship_texture.png')
 gravitation = 0
-number_gravitation = 0
+number_gravitation = 1
 
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 30
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        if pygame.mouse.get_pressed()[0] and gravitation < 0:
+            gravitation += 20
 
     gravitation -= 1
 
     sc.fill((0, 0, 0))
-    if number_gravitation % 2 == 0:
-        number_gravitation += 0.2
+    if number_gravitation % 1 == 0:
+        number_gravitation += 1
         airship.rect.y -= gravitation
     else:
-        number_gravitation += 0.2
-    pygame.display.update()
+        number_gravitation += 1
     sc.blit(airship.image, airship.rect)
     clock.tick(FPS)
     pygame.display.update()
