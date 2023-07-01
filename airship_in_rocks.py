b92cf26806d9acc03597629600b15d4342ea7c48
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2023-07-01 17:07:04 +0300

    i dont Knov
    :P

--- airship_in_rocks.py
+++ airship_in_rocks.py
@@ -45,12 +45,23 @@ class AirShip(pygame.sprite.Sprite):
         self.original_image = self.image
         self.original_rect = self.rect.copy()
 
+class ObstrucTion(pygame.sprite.Sprite):
+    def __init__(self, x, y, filename):
+        self.image = pygame.image.load(filename).convert()
+        self.image.set_colorkey((0, 0, 0))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*5, self.image.get_height()*5))
+        self.rect = self.image.get_rect(center=(x, y))
+
 airship = AirShip(50, 250, 'airship_texture.png')
+airship_mask = pygame.mask.from_surface(airship.image)
 airship_y = 0
 direction = 0
 gravitation = 0
 number_gravitation = 1
 rotate = 1
+
+obstruction = ObstrucTion(800, 450, 'obstruction_texture.png')
+obstruction_mask = pygame.mask.from_surface(obstruction.image)
 
 clock = pygame.time.Clock()
 FPS = 30
@@ -63,22 +74,20 @@ while 1:
     speed_fall()
     touch()
 
-    # if gravitation < 6 and direction >= 10:
-    #     airship_y = airship.rect.y
-    #     airship.image = pygame.transform.rotate(airship.original_image, -5)  # Поворот в обратном направлении на 5 градусов
-    #     airship.rect = airship.image.get_rect(center=airship.rect.center)  # Обновление rect с новым положением
-    #     direction -= 5
-    # if gravitation > 3 and direction <= 10:
-    #     airship_y = airship.rect.y
-    #     airship.image = pygame.transform.rotate(airship.original_image, 5)  # Поворот на 5 градусов
-    #     airship.rect = airship.image.get_rect(center=airship.rect.center)  # Обновление rect с новым положением
-    #     direction += 5
     smoothness_rotate()
 
 
     sc.fill((0, 0, 0))
 
-
     sc.blit(airship.image, airship.rect)
+    sc.blit(obstruction.image, obstruction.rect)
+    pygame.draw.rect(sc, (255, 255, 255), pygame.Rect(airship), 1)
     clock.tick(FPS)
     pygame.display.update()
+    obstruction.rect.x -= 13
+    if obstruction.rect.right < 0:
+        obstruction.rect.x = 800
+        obstruction.rect.bottom = 500
+    offset = (obstruction.rect.x - airship.rect.x, obstruction.rect.y - airship.rect.y)
+    if airship_mask.overlap_area(obstruction_mask, offset) > 0:
+        print('пересечение')
